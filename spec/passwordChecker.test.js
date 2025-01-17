2143120d3ddf0c6d0d30e30cb3e8dc8246dfb0e8
Hadje <hadje.abakar@student.vinci.be>
2025-01-17 17:39:33 +0100

    refactor: moved main initialization

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,24 +1,19 @@
 import { Main } from '../src/passwordChecker.js';
 
 describe("simple test function should return ok", function() {
-    
-    it("should return ok", function() {
-        let main = new Main();
+    let main = new Main();
 
+    it("should return ok", function() {
         let result = main.test("");
 
         expect(result).toBe("ok");
     });
     it("should verify if the email contains at least 8 caracthers", function() {
-        let main = new Main();
-
         let result = main.emailChercker("test@gmail.com");
 
         expect(result).toBe(true);
     });
     it("should verify if the email contains at least 8 caracthers", function() {
-        let main = new Main();
-
         let result = main.emailChercker("test@");
 
         expect(result).toBe(false);
